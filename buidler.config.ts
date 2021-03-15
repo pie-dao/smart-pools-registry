70cf03168551b15a026ba7fc0e4ce3d36c19e150
Mick de Graaf <mick@mickdegraaf.nl>
2021-03-15 18:14:10 +0100

    deploy task + bsc support

--- buidler.config.ts
+++ buidler.config.ts
@@ -1,7 +1,11 @@
 require("dotenv").config();
-import { BuidlerConfig, usePlugin } from "@nomiclabs/buidler/config";
+import { BuidlerConfig, task, usePlugin } from "@nomiclabs/buidler/config";
+import {deployContract} from "ethereum-waffle";
+import RegistryArtifact from "./artifacts/SmartPoolRegistry.json";
 
-usePlugin("buidler-deploy");
+// usePlugin("buidler-deploy");
+usePlugin("@nomiclabs/buidler-ethers");
+usePlugin("bsc-buidler-etherscan");
 
 interface ExtendedBuidlerConfig extends BuidlerConfig {
     [x:string]: any
@@ -23,10 +27,24 @@ const config: ExtendedBuidlerConfig = {
       forked: {
         url: "http://127.0.0.1:8545",
       },
+      bsc: {
+        url: `https://bsc-dataseed.binance.org/`,
+        accounts: [process.env.BSC_PRIVATE_KEY],
+      },
     },
     namedAccounts: {
       deployer: 0,
     },
+    etherscan: { apiKey: process.env.ETHERSCAN_KEY  }
 }
+
+
+task("deploy-registry", async(taskArgs, {ethers}) => {
+  const signers = await ethers.getSigners();
+
+  const oven = await deployContract(signers[0], RegistryArtifact);
+
+  console.log(`Contract deployed at: ${oven.address}`);
+});
 
 export default config;
